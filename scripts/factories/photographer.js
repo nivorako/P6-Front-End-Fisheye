b5d89c35185ef33ccc49d62df5e954b1c132e947
nivorako <nivo.rakoto@yahoo.fr>
2022-07-21 12:47:28 +0200

    show pages, fetch data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,34 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+export function photographerFactory(data) {
+    const { name, portrait, city, country, tagline } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+
+        const link = document.createElement('a');
+        link.setAttribute("href", "photographer.html");
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute('alt', `photographie de ${name}`)
+
+        const cityElt = document.createElement('p');
+        cityElt.textContent = `${city}, ${country}`;
+
+        const tag = document.createElement('p');
+        tag.innerHTML = tagline;
+        
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const article = document.createElement( 'article' );
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(cityElt);
+        article.appendChild(tag)
+        link.appendChild(article);
+        return (link);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture,city ,country ,tagline, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,19 @@
+import { photographerFactory } from "../factories/photographer.js"
+
 async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
-    const photographers = [
-        {
-            "name": "Ma data test",
-            "id": 1,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Ceci est ma data test",
-            "price": 400,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Autre data test",
-            "id": 2,
-            "city": "Londres",
-            "country": "UK",
-            "tagline": "Ceci est ma data test 2",
-            "price": 500,
-            "portrait": "account.png"
-        },
-    ]
-    // et bien retourner le tableau photographers seulement une fois
-    return ({
-        photographers: [...photographers, ...photographers, ...photographers]})
+   const photographersApi = fetch("/data/photographers.json")
+                                .then(res => res.json())
+                                .then(function(res) {
+                                    let photographers = res.photographers
+                                    let media = res.media
+                                    return {
+                                        photographers,
+                                        media
+                                    }
+                                })
+                                .catch(err => console.log("Error to occur: ", err))
+    console.log("photographersApi: ", photographersApi)                          
+    return photographersApi
 }
 
 async function displayData(photographers) {
@@ -37,7 +28,9 @@ async function displayData(photographers) {
 
 async function init() {
     // Récupère les datas des photographes
+    
     const { photographers } = await getPhotographers();
+    
     displayData(photographers);
 };
 
