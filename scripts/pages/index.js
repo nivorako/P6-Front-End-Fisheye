2a6e9224f24ea76e4878bf1d5d3576ab77364e08
nivorako <nivo.rakoto@yahoo.fr>
2022-08-10 16:46:02 +0200

    fix stuff

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -156,6 +156,7 @@ async function init() {
             // on ne peut cliquer qu une seule fois : data-increment="true"
             likeIncrements[i].addEventListener("click", () => {
                 if(likeIncrements[i].getAttribute("data-increment") === 'false'){
+                    console.log('data === false, donc incremente')
                     photographerLikes[i].textContent++;
                     likesElt.textContent++;
                     likeIncrements[i].setAttribute('data-increment', 'true');
