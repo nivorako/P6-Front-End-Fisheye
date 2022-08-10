932fd6099dd7d01db08469cb93f62f2bb647337b
nivorako <nivo.rakoto@yahoo.fr>
2022-08-10 14:05:11 +0200

    factorise sorter

--- scripts/factories/sorter.js
+++ scripts/factories/sorter.js
@@ -1,5 +1,38 @@
 export function sorter(){
     const sorterWrapper = document.querySelector('.sorter')
+
+    function sorterOnClick (){
+        const sorterItems = document.querySelectorAll('.sorter__item');
+        const sorterItemsLength = sorterItems.length;
+        const sorterSelected = document.querySelector('.sorter__selected');
+        const btnSelected = document.querySelector('.fa-angle-down');
+       
+
+        // chaque fois qu on clicke sur sorter selected
+        sorterItems[0].addEventListener('click', () => {
+            
+            for( let i=0; i<sorterItemsLength; i++){
+                sorterItems[i].classList.toggle('active')
+            }
+            btnSelected.classList.toggle('active'); 
+        })
+
+        // le item selecté s'affiche dans sorter__sort
+        sorterItems.forEach(item => {
+            item.addEventListener('click', () => {
+                let x;
+                let selectedItem = item.querySelector('.sorter__sort').innerHTML;
+                x=sorterSelected.innerHTML;
+                sorterSelected.innerHTML = selectedItem;
+                item.querySelector('.sorter__sort').innerHTML = x;
+                console.log('selectedItem: ', selectedItem)
+            })
+    
+        })
+        
+    }
+
+    
     const sorter = /*html*/`
         <div class="sorter__container">
             <h2 class="sorter__title">Trier par</h2>
@@ -20,5 +53,6 @@ export function sorter(){
 
     sorterWrapper.innerHTML = sorter
 
+    sorterOnClick()
     return sorterWrapper
 }
