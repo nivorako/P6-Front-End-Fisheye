f7ec2c2bb4424c55ed7049ce8e98c19860a96834
nivorako <nivo.rakoto@yahoo.fr>
2022-09-06 14:44:54 +0200

    set validate first name and last name

--- scripts/utils/modal.js
+++ scripts/utils/modal.js
@@ -18,7 +18,7 @@ export function displayModal(photographer){
     const modalElement = /*html*/`
         <header class="modal__header">
             <div class="modal__function">
-                <h1 id="modalTitle">Contactez-moi</h1>
+                <h1 id="modalTitle ">Contactez-moi</h1>
                 <p class="modal__name">${name}</p>
             </div>
             <img src="assets/icons/close.svg" class="modal__close" tabindex="0" role="button" aria-label="fermer le modal"/>
@@ -27,8 +27,10 @@ export function displayModal(photographer){
         <form class="modal__form">
                 <label for="lastName" id="nom">Nom</label>
                 <input  class="input" type="text" name="lastname" id="lastName" tabindex="0" aria-labelledby="nom"/>
+                <span class="lastNameError hidden">mettez au moins deux caractères</span>
                 <label for="firstName" >Prénom</label>
                 <input  class="input" type="text" name="firstName" id="firstName" tabindex="0">
+                <span class="firstNameError hidden">mettez au moins deux caractères</span>
                 <label for="email">email</label>
                 <input  class="input" type="email" name="email" id="email" tabindex="0">
                 <div class="modal__comment">
--- scripts/utils/submit.js
+++ scripts/utils/submit.js
@@ -6,24 +6,49 @@ export function submitForm(){
     const firstName = document.getElementById('firstName');
     const email = document.getElementById('email');
 
-    //const REGEX pour nom prenom
-    const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ \-]+$/
-
-
     console.log('lastname: ', lastName.value);
     console.log('firstName: ', firstName.value);
     console.log('email: ', email.value);
 
-     if (firstName.value.trim().length >= 2 && firstName.value.trim().match(regex)){
-        console.log("firstName ok!!")
-       
+    if(validateFirstName() && validateLastName()){
+        closeModal();
+    }else{
+        
+    }
+}
+
+function validateFirstName(){
+    //const REGEX pour nom prenom
+    const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ \-]+$/
+
+    const firstNameError = document.querySelector('.firstNameError')
+    const firstName = document.getElementById('firstName');
+    if (firstName.value.trim().length >= 2 && firstName.value.trim().match(regex)){
+        if(!firstNameError.classList.contains('hidden')){
+            firstNameError.classList.add('hidden')
+        }
         return true
     }else{
         console.log("firstName not ok!!")
+        firstNameError.classList.remove('hidden')
         return false
     }
+}
 
+function validateLastName(){
+    //const REGEX pour nom prenom
+    const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ \-]+$/
 
-    closeModal();
-    
+    const lastNameError = document.querySelector('.lastNameError')
+    const lastName = document.getElementById('lastName')
+    if (lastName.value.trim().length >= 2 && lastName.value.trim().match(regex)){
+        if(!lastNameError.classList.contains('hidden')){
+            lastNameError.classList.add('hidden')
+        }
+        return true
+    }else{
+        console.log("lastName not ok!!")
+        lastNameError.classList.remove('hidden')
+        return false
+    }
 }
